171a2bcaedf98e38cc6c45e76014143fa6973295
Rakesh <chnaveen7496@gmail.com>
2019-03-23 12:49:22 +0530

    changes for new project

---
+++ Find a Doctor/Controllers/SettingsVC/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  Find a Doctor
+//
+//  Created by praveen dole on 3/22/19.
+//  Copyright © 2019 Calibrage Info System Pvt Limitd. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         self.navigationItem.title = "Settings"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
